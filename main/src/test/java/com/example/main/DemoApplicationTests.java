6b482a9f202c87ea15ff511a9d2388d76368361e
Thom Hurks <thomhurks@gmail.com>
2021-05-26 00:13:52 +0200

    Fix

--- main/src/test/java/com/example/main/DemoApplicationTests.java
+++ main/src/test/java/com/example/main/DemoApplicationTests.java
@@ -2,11 +2,13 @@ package com.example.main;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import javax.transaction.Transactional;
 
 @SpringBootTest
 @Transactional
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 class DemoApplicationTests {
 
 	@Test
